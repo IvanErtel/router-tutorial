b782d6e5f5a468bed19c1fa900fe59e603e3b6f7
Ivan Ertel <alanertel2275@gmail.com>
2022-06-09 15:13:30 -0300

    routesReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Link } from "react-router-dom";
 
-function App() {
+/*importamos Link, Link es similar a la etiqueta <a> en HTML pero con la diferencia que 
+con link no recarga toda la pagina si no el modulo al cual nos referimos, en vez de utilizar
+href usamos to="(ruta)"*/
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bienvenidos!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+/*BrowserRouter nos deja navegar entre paginas/importamos Routes y dentro incorporamos las 
+diferentes rutas con sus respectivos componentes a mostrar (Route lleva 2 parametros, 
+  path=(nombre de la ruta) y element=(el modulo que se va a mostrar))*/
+
+ <BrowserRouter>
+    <Routes>
+  <Route path="/" element={<App />}>
+    <Route path="expenses" element={<Expenses />} />
+    <Route path="invoices" element={<Invoices />}>
+      <Route
+        index
+        element={
+          <main style={{ padding: "1rem" }}>
+            <h3>Seleccione su factura</h3>
+          </main>
+        }
+      />
+      <Route path=":invoiceId" element={<Invoice />} />
+    </Route> 
+    {/* en caso de no encontrar la ruta se mostrara el siguiente <p></p> (tambien se
+      podria crear otro componente con una pagina y importarlo a esta Route ) */}
+    <Route
+      path="*"
+      element={
+        <main style={{ padding: "1rem", background: "#c8c32f" }}>
+          <h1>404 NOT FOUND!</h1>
+        </main>
+      }
+    />
+  </Route>
+</Routes>
+  </BrowserRouter>
+);
